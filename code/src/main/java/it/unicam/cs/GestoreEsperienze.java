5d5eedc3b2bc122bf972cd1c49bc75f17ee577d0
damiano00 <damiano.pasquini@studenti.unicam.it>
2022-02-02 10:23:13 +0100

    definizione firma metodi di GestoreEsperienze, Pagamento e Tag

--- code/src/main/java/it/unicam/cs/GestoreEsperienze.java
+++ code/src/main/java/it/unicam/cs/GestoreEsperienze.java
@@ -1,4 +1,30 @@
 package it.unicam.cs;
 
+import java.util.ArrayList;
+
 public class GestoreEsperienze {
+
+    // todo: add instance variables
+
+    GestoreEsperienze(){
+        // todo constructor
+    }
+
+    public void prenotaEsperienza(Esperienza esperienza){
+        // todo
+    }
+
+    public void creaRiepilogo(Esperienza esperienza, String titolo, String descrizione, int prezzo){
+        // todo
+    }
+
+    public void aggiungiEsperienza(Esperienza esperienza){
+        // todo
+    }
+
+    public ArrayList<Cicerone> checkCiceroniDisponibili(Esperienza esperienza){
+        // todo
+        return null;
+    }
+
 }
--- code/src/main/java/it/unicam/cs/Pagamento.java
+++ code/src/main/java/it/unicam/cs/Pagamento.java
@@ -1,4 +1,29 @@
 package it.unicam.cs;
 
 public class Pagamento {
+
+    private final int importo;
+    private final Turista turista;
+    private final Esperienza esperienza;
+
+    Pagamento(int importo, Turista turista, Esperienza esperienza){
+        this.importo = importo;
+        this.turista = turista;
+        this.esperienza = esperienza;
+    }
+
+    // todo add other methods to complete a payment
+
+    public int getImporto(){
+       return this.importo;
+    }
+
+    public Turista getTurista(){
+        return this.turista;
+    }
+
+    public Esperienza getEsperienza(){
+        return this.esperienza;
+    }
+
 }
