b708a54525b3f6f09f5f739b2721ba4caa6e4468
damiano00 <damiano.pasquini@studenti.unicam.it>
2022-02-16 11:27:33 +0100

    modifica metodi gestore toponimi

--- code/src/main/java/it/unicam/cs/GestoreToponimi.java
+++ code/src/main/java/it/unicam/cs/GestoreToponimi.java
@@ -12,7 +12,33 @@ public class GestoreToponimi {
         return false;
     }
 
+    /**
+     * Aggiunge un nuovo toponimo al DB
+     * @param toponimoFiglio toponimo figlio
+     * @param toponimoGenitore toponimo genitore
+     */
     public void registraToponimo(String toponimoFiglio, String toponimoGenitore){
         //todo implements
     }
+
+    /**
+     * Controlla che il toponimo esista nel DB
+     * @param toponimoDaEliminare toponimo da eliminare
+     * @return true se il toponimo esiste nel DB, false altrimenti
+     */
+    public boolean controllaEsistenzaToponimo(String toponimoDaEliminare){
+        //todo implement
+        return false;
+    }
+
+    /**
+     * Controlla che il toponimo in input non abbia figli
+     * @param toponimoDaEliminare toponimo da eliminare
+     * @return true se ha figli, false altrimenti
+     */
+    public boolean controllaEsistenzaFigli(String toponimoDaEliminare){
+        //todo implement
+        return false;
+    }
+
 }
