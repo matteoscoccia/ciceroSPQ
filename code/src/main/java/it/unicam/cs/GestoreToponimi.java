1350863c781ba8e640712e4e1ac1cfcfb22600fd
damiano00 <damiano.pasquini@studenti.unicam.it>
2022-02-16 11:11:53 +0100

    aggiunta classi gestore toponimi e gestore tag

---
+++ code/src/main/java/it/unicam/cs/GestoreToponimi.java
@@ -0,0 +1,18 @@
+package it.unicam.cs;
+
+public class GestoreToponimi {
+
+    /**
+     * Controlla se il genitore è esistente nel DB
+     * @param toponimoDaControllare toponimo da controllare
+     * @return true se il toponimo esiste nel DB, false altrimenti
+     */
+    public boolean controllaToponimoGenitore(String toponimoDaControllare){
+        //todo implements
+        return false;
+    }
+
+    public void registraToponimo(String toponimoFiglio, String toponimoGenitore){
+        //todo implements
+    }
+}
