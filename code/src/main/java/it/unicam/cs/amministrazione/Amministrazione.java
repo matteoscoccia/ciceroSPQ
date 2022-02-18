2fb3e10d689de3855acf5f89f086efd81c3ae7dc
damiano00 <damiano.pasquini@studenti.unicam.it>
2022-02-18 17:59:52 +0100

    implementazione casi d'uso: - aggiungere toponimi - eliminare toponimi (terminare implementazione query nel DBManager.java)

--- code/src/main/java/it/unicam/cs/amministrazione/Amministrazione.java
+++ code/src/main/java/it/unicam/cs/amministrazione/Amministrazione.java
@@ -4,8 +4,10 @@ import it.unicam.cs.esperienza.Esperienza;
 import it.unicam.cs.utente.GestoreAccount;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import static it.unicam.cs.main.App.gestoreEsperienze;
+import static it.unicam.cs.main.App.gestoreToponimi;
 
 public class Amministrazione {
 
@@ -19,11 +21,23 @@ public class Amministrazione {
         GestoreAccount.adminEliminaAccount();
     }
 
+    //todo correggere
     public void eliminaEsperienza(Esperienza esperienzaDaEliminare){
         gestoreEsperienze.rimuoviEsperienza(esperienzaDaEliminare,this);
     }
 
     public void effettuaRicerca(){
         gestoreEsperienze.ricercaConFiltri();
+    }
+
+    public void aggiungiToponimo() throws SQLException {
+        gestoreToponimi.registraToponimo();
+    }
+
+    public void eliminaToponimo() throws SQLException{
+        System.out.println("Specificare il nome del toponimo da eliminare");
+        Scanner scanner = new Scanner(System.in);
+        String toponimoDaEliminare = scanner.nextLine();
+        gestoreToponimi.eliminaToponimo(toponimoDaEliminare);
     }
 }
